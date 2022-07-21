7d8ec502910e92090e3e9ae1ef648a1b832663f9
alorslouis <alorslouis@users.noreply.github.com>
2022-07-20 21:19:40 -0700

    +: component skeletons; c: correct html; c: styleBoops

--- pages/components/layout.tsx
+++ pages/components/layout.tsx
@@ -1,21 +1,24 @@
 import Link from "next/link";
-import Image from "next/image";
-import styles from "../styles/globals.module.css";
 
 export default function Layout({ children }: any) {
   return (
     <>
-      <div className="container mx-auto min-h-screen">
-        <div className="text-center py-4 ">
+      <div className="container mx-auto min-h-max">
+        <nav className="text-center py-4 ">
           <Link href="/">
-            <h1 className="text-bold font-black text-4xl text-center">
+            <h1 className="text-bold font-black text-4xl text-center cursor-pointer">
               LILLIES
             </h1>
           </Link>
-          <div className="text-xs">BCN • LDN • SAN</div>
+          <p className="text-xs">BCN • LDN • SAN</p>
+          <div className="flex justify-evenly py-4">
+            <p>cat2</p>
+            <p>cat2</p>
+            <p>cat2</p>
+          </div>
           {/* <p className="text-xl">custom couture</p> */}
-        </div>
-        <main className="text-center h-full">{children}</main>
+        </nav>
+        <main className="text-center h-full ">{children}</main>
 
         <footer className="text-center">
           <p>footer</p>
