9480649604a7ff76108a9d03c98098270d0976b2
alorslouis <alorslouis@users.noreply.github.com>
2022-08-04 15:24:41 -0700

    +: [about] - studio Img

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,9 +1,21 @@
+import Image from "next/image";
+
 function About() {
   return (
     <>
       <div className="items-center my-10">
         <div className="font-semibold text-lg">hecho en barcelona</div>
         <div>par hamez</div>
+        <div className="p-8 m-8 rounded-lg border border-white">
+          <Image
+            src={"/IMG_8860.jpeg"}
+            width={100}
+            height={100}
+            layout={"responsive"}
+            alt="the studio - barcelona es"
+            className="rounded-md"
+          />
+        </div>
       </div>
     </>
   );
