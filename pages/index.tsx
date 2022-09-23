a1b53332df584a54c9efb684216f9c90416ac4c4
alorslouis <alorslouis@users.noreply.github.com>
2022-09-22 20:42:47 -0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,36 +4,55 @@ import Image from "next/image";
 import Link from "next/link";
 import { useEffect } from "react";
 import styles from "../styles/Home.module.css";
-import type { Record, AirRecords } from "./products/[name]";
+import type { AirRecords, Attachment } from "./products/[name]";
 import heroImage from "../public/IMG_8603-PhotoRoom.png";
 
-export interface AirProps {
-  recs: {
-    records: Record[];
-  };
+export interface Record {
+  id: string;
+  createdTime: string;
+  fields: Fields;
 }
 
-// function to return unqiue values from a nested object array
+export interface Fields {
+  Images: Attachment[];
+  Caption: string;
+  linkTo: string;
+  isActive: boolean;
+}
 
-// export const getStaticProps: GetStaticProps = async () => {
-//   const res = await fetch(
-//     `https://api.airtable.com/v0/${process.env.base_id}/products/?api_key=${process.env.api_key}`
-//   );
-//   const recs = (await res.json()) as AirRecords;
+export const getStaticProps: GetStaticProps = async () => {
+  const res = await fetch(
+    `https://api.airtable.com/v0/${process.env.base_id}/Sections/?api_key=${process.env.api_key}`
+  );
+  // const records = (await res.json()) as Record[];
+  const records = await res.json();
 
-//   return {
-//     props: {
-//       recs,
-//     },
-//   };
-// };
+  return {
+    props: {
+      records,
+    },
+  };
+};
 
-const Home: NextPage<AirProps> = () => {
+interface PageProps {
+  records: {
+    record: Record;
+  }[];
+}
+
+const Home: NextPage<PageProps> = ({ records }) => {
   // const products = recs;
   // console.log(products);
   // console.log(products.records[0]);
   // console.log(recs.records);
   // console.log(dummy);
+
+  console.log(records);
+
+  const x = records[0];
+
+  console.log(x);
+
   return (
     <>
       <div className="container text-center">
@@ -49,6 +68,7 @@ const Home: NextPage<AirProps> = () => {
         </ul> */}
 
         {/* <div>{records[0].fields.brand}</div> */}
+
         <h1 className="font-thin">LANDING PAGE PLACEHOLDER</h1>
         <div className="items-center justify-center mx-auto my-12 flex flex-col md:flex-row">
           <Image src={heroImage} layout="intrinsic" />
