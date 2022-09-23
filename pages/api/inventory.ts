2c2c568887f36b0a5b99cf1b61455a3e778060c6
alorslouis <alorslouis@users.noreply.github.com>
2022-09-22 20:42:38 -0700

    Create inventory.ts
    to respond to snipcart order.completed webhook

---
+++ pages/api/inventory.ts
@@ -0,0 +1,7 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+import { AirRecords, Record } from "../products/[name]";
+
+// export default async (req: NextApiRequest, res: NextApiResponse) => {
+//   res.status(200).json(products);
+// };
