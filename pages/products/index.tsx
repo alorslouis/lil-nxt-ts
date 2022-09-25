6e7936662d82b9a816ebcad36d282d18cb0384b7
alorslouis <alorslouis@users.noreply.github.com>
2022-09-25 14:45:16 -0700

    Update index.tsx
    +: hover bar for navigating to categories - currently floating left, will see how it feels and whether to port to other pages

--- pages/products/index.tsx
+++ pages/products/index.tsx
@@ -34,22 +34,28 @@ const Product: NextPage<AirProps> = ({ recs }) => {
   return (
     <div>
       {/* category nav bar */}
-      <div className="flex my-8 justify-evenly flex-wrap">
-        {jj.map((j) => {
-          return (
-            <div key={j} className="text-xl font-extralight cursor-pointer">
-              <Link href={`products/category/${j}`}>{j}</Link>
-            </div>
-          );
-        })}
+      <hr />
+      <div className="flex sticky top-0 z-10">
+        <div className="flex flex-col gap-4 flex-wrap sticky top-0 right-0">
+          {jj.map((j) => {
+            return (
+              <div
+                key={j}
+                className="text-md bg-slate-800 dark:bg-slate-100 bg-opacity-10 dark:bg-opacity-10 px-4 py-2 first:mt-2 border-2 hover:border-r-slate-400 rounded-r-lg font-extralight cursor-pointer transition-all ease-in-out"
+              >
+                <Link href={`products/category/${j}`}>{j}</Link>
+              </div>
+            );
+          })}
+        </div>
       </div>
       <ul className="grid grid-cols-2 lg:grid-cols-3 gap-4 p-2 mx-auto max-w-screen-md">
         {products &&
           products.records.map((record) => (
-            <li key={record.id} className="flex flex-grow ">
+            <li key={record.id} className="flex flex-col flex-grow ">
               <Link href={`/products/${record.fields.route}`}>
-                <div className="flex flex-col flex-1 cursor-pointer mx-8 mt-auto p-2 self-center rounded-3xl hover:-translate-y-1 transition ease-in-out hover:shadow-lg active:translate-y-1 active:shadow-lg">
-                  <div>
+                <div className="flex flex-col flex-1 cursor-pointer mx-8 mt-auto p-2 px-12 self-center rounded-3xl hover:-translate-y-1 transition ease-in-out hover:shadow-lg active:translate-y-1 active:shadow-lg">
+                  <div className="mb-auto">
                     {record.fields?.attach ? (
                       <Image
                         key={record.fields.attach[0].url}
