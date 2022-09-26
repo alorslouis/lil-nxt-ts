979f53c85a5168b5802bcd51c09c291d3e8da1dc
alorslouis <alorslouis@users.noreply.github.com>
2022-09-25 20:52:24 -0700

    Update inventory.ts

--- pages/api/inventory.ts
+++ pages/api/inventory.ts
@@ -5,27 +5,48 @@ import { AirRecords, Record } from "../products/[name]";
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   console.log(req.body);
 
-  const x = req.body.content.items.map((item: any) => {
+  const x: {
+    id: "string";
+    fields: {
+      inventory: number;
+    };
+  }[] = req.body.content.items.map((item: any) => {
     return {
       id: item.id,
       fields: {
-        quantity: 0,
+        inventory: 0,
       },
     };
   });
   const xx = { records: x };
-  const up = async () => {
-    await fetch(`https://api.airtable.com/v0/${process.env.base_id}/products`, {
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: "Bearer " + process.env.api_key,
-      },
-      method: "PATCH",
-      body: JSON.stringify(x),
-    });
-  };
 
-  up();
+  x.forEach(async (element: any) => {
+    const y = element.id;
+    const call = await fetch(
+      `https://api.airtable.com/v0/${process.env.base_id}/products/${y}`,
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + process.env.api_key,
+        },
+        method: "PATCH",
+        body: JSON.stringify(xx),
+      }
+    );
+  });
 
-  res.status(200).json({ d: "d", x, up });
+  // const up = async () => {
+  //   await fetch(`https://api.airtable.com/v0/${process.env.base_id}/products`, {
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //       Authorization: "Bearer " + process.env.api_key,
+  //     },
+  //     method: "PATCH",
+  //     body: JSON.stringify(xx),
+  //   });
+  // };
+
+  // up();
+
+  res.status(200).json({ d: "d", x });
 };
