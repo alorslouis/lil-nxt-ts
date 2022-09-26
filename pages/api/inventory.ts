76d7ab1bcb9a7f97581584c7c9c981915c7a41f1
alorslouis <alorslouis@users.noreply.github.com>
2022-09-25 17:12:45 -0700

    Update inventory.ts

--- pages/api/inventory.ts
+++ pages/api/inventory.ts
@@ -2,6 +2,6 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 import { AirRecords, Record } from "../products/[name]";
 
-// export default async (req: NextApiRequest, res: NextApiResponse) => {
-//   res.status(200).json(products);
-// };
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  res.status(200).json({ d: "d" });
+};
