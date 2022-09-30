bf75dcc5d5b2887efaa25ac1af755f580b89338a
alorslouis <alorslouis@users.noreply.github.com>
2022-09-30 16:58:56 -0700

    Update index.tsx
    c: moved Link to encompass all of the category tag on nav, rather than just inner text

--- pages/products/index.tsx
+++ pages/products/index.tsx
@@ -39,12 +39,11 @@ const Product: NextPage<AirProps> = ({ recs }) => {
         <div className="flex flex-col gap-4 flex-wrap sticky top-0 right-0 -mb-20">
           {jj.map((j) => {
             return (
-              <div
-                key={j}
-                className="text-md bg-slate-800 dark:bg-slate-100 border-l-0 bg-opacity-10 dark:bg-opacity-10 px-4 py-2 first:mt-2 border-2 hover:border-r-slate-400 rounded-r-lg font-extralight cursor-pointer transition-all ease-in-out"
-              >
-                <Link href={`products/category/${j}`}>{j}</Link>
-              </div>
+              <Link key={j} href={`products/category/${j}`}>
+                <div className="text-md bg-slate-800 dark:bg-slate-100 border-l-0 bg-opacity-10 dark:bg-opacity-10 px-4 py-2 first:mt-2 border-2 hover:border-r-slate-400 rounded-r-lg font-extralight cursor-pointer transition-all ease-in-out">
+                  {j}
+                </div>
+              </Link>
             );
           })}
         </div>
