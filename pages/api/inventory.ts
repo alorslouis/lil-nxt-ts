46f73d65f2887451bea8a9e6930ce4e6a92da265
alorslouis <alorslouis@users.noreply.github.com>
2022-10-01 15:46:24 -0700

    Update inventory.ts

--- pages/api/inventory.ts
+++ pages/api/inventory.ts
@@ -3,7 +3,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { AirRecords, Record } from "../products/[name]";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  console.log(req.body.content);
+  console.log(req.body.content.items);
 
   // if (req.body.eventName === "order.completed") {
   const bodyItems = req.body.content.items;
