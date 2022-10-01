2fa4a1567cfe06d880bd16733a2c258c755dc131
alorslouis <alorslouis@users.noreply.github.com>
2022-10-01 12:44:57 -0700

    Update inventory.ts

--- pages/api/inventory.ts
+++ pages/api/inventory.ts
@@ -5,12 +5,14 @@ import { AirRecords, Record } from "../products/[name]";
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   console.log(req.body);
 
-  const x: {
+  const bodyItems = req.body.content.items;
+
+  let x: {
     id: string;
     fields: {
       inventory: number;
     };
-  }[] = req.body.content.items.map((item: any) => {
+  }[] = bodyItems.map((item: any) => {
     return {
       id: item.id,
       fields: {
@@ -31,12 +33,20 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     // redirect: "follow",
   };
 
-  const cc = () =>
-    fetch(
-      // `https://api.airtable.com/v0/${process.env.base_id}/products/${prod.id}?api_key=${process.env.api_key}`,
-      `https://api.airtable.com/v0/${process.env.base_id}/products/?api_key=${process.env.api_key}`,
-      reqOptions
-    );
+  const cc = () => {
+    if (x.length > 1) {
+      fetch(
+        // `https://api.airtable.com/v0/${process.env.base_id}/products/${prod.id}?api_key=${process.env.api_key}`,
+        `https://api.airtable.com/v0/${process.env.base_id}/products/?api_key=${process.env.api_key}`,
+        reqOptions
+      );
+    } else {
+      fetch(
+        `https://api.airtable.com/v0/${process.env.base_id}/products/${x[0].id}?api_key=${process.env.api_key}`,
+        reqOptions
+      );
+    }
+  };
   // .then((response) => response.text())
   // .then((result) => e.push(result))
   // .catch((error) => console.log("error", error));
