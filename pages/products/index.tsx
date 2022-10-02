03a10ec20b0d729ab33e0d39d214ec3d5bb154e4
alorslouis <alorslouis@users.noreply.github.com>
2022-10-02 15:35:11 -0700

    c: [products/index] - change sidenav stacking context
    c: - change sidenav stacking context - was previously sticky with negative margin but that was fucking up with more categories being added; now is in a flex - need to fix sticky

--- pages/products/index.tsx
+++ pages/products/index.tsx
@@ -35,55 +35,56 @@ const Product: NextPage<AirProps> = ({ recs }) => {
     <div>
       {/* category nav bar */}
       <hr />
-      <div className="flex sticky top-0 z-10">
-        <div className="flex flex-col gap-4 flex-wrap sticky top-0 right-0 -mb-20">
+      <div className="flex">
+        {/* <div className="flex sticky top-0 z-10"> */}
+        <div className="flex flex-col gap-4 flex-wrap sticky top-0 -mr-12">
           {jj.map((j) => {
             return (
               <Link key={j} href={`products/category/${j}`}>
-                <div className="text-md font-futura bg-slate-800 dark:bg-slate-100 border-l-0 bg-opacity-10 dark:bg-opacity-10 px-4 py-2 first:mt-2 border-2 hover:border-r-slate-400 rounded-r-lg font-extralight cursor-pointer transition-all ease-in-out">
+                <div className="text-md flex font-futura bg-slate-800 dark:bg-slate-100 border-l-0 bg-opacity-10 dark:bg-opacity-10 px-4 py-2 first:mt-2 border-2 hover:border-r-slate-400 rounded-r-lg font-extralight cursor-pointer transition-all ease-in-out">
                   {j}
                 </div>
               </Link>
             );
           })}
         </div>
-      </div>
-      <ul className="grid grid-cols-2 lg:grid-cols-3 gap-2 p-2 mx-auto max-w-screen-md">
-        {products &&
-          products.records.map((record) => (
-            <li key={record.id} className="flex ">
-              <Link href={`/products/${record.fields.route}`}>
-                <div className="flex flex-col grow flex-1 cursor-pointer mx-2 px-12 mt-auto p-2  self-center rounded-3xl hover:-translate-y-1 transition ease-in-out hover:shadow-lg active:translate-y-1 active:shadow-lg">
-                  <div className="mb-auto">
-                    {record.fields?.attach ? (
-                      <Image
-                        key={record.fields.attach[0].url}
-                        alt={record.fields.title}
-                        src={record.fields.attach[0].url}
-                        width={record.fields.attach[0].width}
-                        height={record.fields.attach[0].height}
-                        layout="responsive"
-                        unoptimized
-                      />
-                    ) : (
-                      <Image
-                        // key={}
-                        src={"/lilsOg.png"}
-                        width={700}
-                        height={400}
-                        // layout="fill"
-                      />
-                    )}
+        {/* </div> */}
+        <ul className="grid grid-cols-2 lg:grid-cols-3 gap-2 p-2 mx-auto max-w-screen-lg">
+          {products &&
+            products.records.map((record) => (
+              <li key={record.id} className="flex ">
+                <Link href={`/products/${record.fields.route}`}>
+                  <div className="flex flex-col grow flex-1 cursor-pointer mx-2 px-12 mt-auto p-2  self-center rounded-3xl hover:-translate-y-1 transition ease-in-out hover:shadow-lg active:translate-y-1 active:shadow-lg">
+                    <div className="mb-auto">
+                      {record.fields?.attach ? (
+                        <Image
+                          key={record.fields.attach[0].url}
+                          alt={record.fields.title}
+                          src={record.fields.attach[0].url}
+                          width={record.fields.attach[0].width}
+                          height={record.fields.attach[0].height}
+                          layout="responsive"
+                          unoptimized
+                        />
+                      ) : (
+                        <Image
+                          // key={}
+                          src={"/lilsOg.png"}
+                          width={700}
+                          height={400}
+                          // layout="fill"
+                        />
+                      )}
+                    </div>
+                    <p className="font-thin font-nimb capitalize text-sm md:text-base py-6 mt-auto">
+                      {record.fields.title}
+                    </p>
                   </div>
-
-                  <p className="font-thin font-nimb capitalize text-sm md:text-base py-6 mt-auto">
-                    {record.fields.title}
-                  </p>
-                </div>
-              </Link>
-            </li>
-          ))}
-      </ul>
+                </Link>
+              </li>
+            ))}
+        </ul>
+      </div>
     </div>
   );
 };
