40a16c8451cb2ef346d292e007c2f1745581d7d5
alorslouis <alorslouis@users.noreply.github.com>
2022-10-09 16:31:51 -0700

    c: [layout] - fix typing on props (prev: any)

--- pages/components/layout.tsx
+++ pages/components/layout.tsx
@@ -2,9 +2,14 @@ import Head from "next/head";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import Script from "next/script";
-import { browser } from "process";
 
-export default function Layout({ children, pageTitle }: any) {
+export default function Layout({
+  children,
+  pageTitle,
+}: {
+  children: React.ReactNode;
+  pageTitle?: string;
+}) {
   // get the current pathname
   const path = typeof window !== "undefined" ? window.location.pathname : "";
 
