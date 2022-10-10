0167ac387b46e29a46a0c0ca04f89ec459a403f9
alorslouis <alorslouis@users.noreply.github.com>
2022-10-09 20:52:57 -0700

    Update catNav.tsx

--- pages/components/catNav.tsx
+++ pages/components/catNav.tsx
@@ -1,5 +1,7 @@
 import { GetStaticProps } from "next";
 import Router from "next/router";
+import { useEffect, useState } from "react";
+import { AirProps } from "../products/category/[category]";
 import { AirRecords } from "../products/[name]";
 
 export const getStaticProps: GetStaticProps = async () => {
@@ -8,6 +10,10 @@ export const getStaticProps: GetStaticProps = async () => {
   );
   const recs = (await res.json()) as AirRecords;
 
+  //   const res = await fetch("http://localhost:3000/api/products");
+
+  //   const recs = await res.json();
+
   return {
     props: {
       recs,
@@ -15,9 +21,45 @@ export const getStaticProps: GetStaticProps = async () => {
   };
 };
 
-const CatNav = ({ recs }: { recs: AirRecords }) => {
-  console.log(recs);
-  return <div>t</div>;
-};
+export default async function CatNav({ recs }: AirProps) {
+  // const CatNav = () => {
+  // const res = await fetch("/api/products");
+  // const recs = (await res.json()) as ProductApi[];
 
-export default CatNav;
+  console.log({ catNav: recs });
+  return <div>{JSON.stringify(recs)}t</div>;
+}
+
+async function GetRecs() {
+  const res = await fetch("/api/products");
+
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    let ignore = false;
+    fetch("/api/products")
+      .then((response) => response.json())
+      .then((json) => {
+        if (!ignore) {
+          setData(json);
+        }
+      });
+    return () => {
+      ignore = true;
+    };
+  }, []);
+  return data;
+}
+
+// export default CatNav;
+
+interface ProductApi {
+  name: string;
+  id: string;
+  // id: product.fields.title.replace(/\s+/g, "-").toLowerCase(),
+  price: number;
+  description: string;
+  image: string;
+  stock: number;
+  isActive: boolean;
+  category: string;
+}
