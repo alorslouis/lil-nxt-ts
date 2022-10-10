7fa1fedb2d12315c2ebb0fd5841b2ea4876a4412
alorslouis <alorslouis@users.noreply.github.com>
2022-10-10 11:03:53 -0700

    c: [/CatNav] - remove the fetching logic from component

--- pages/components/catNav.tsx
+++ pages/components/catNav.tsx
@@ -4,27 +4,29 @@ import { useEffect, useState } from "react";
 import { AirProps } from "../products/category/[category]";
 import { AirRecords } from "../products/[name]";
 
-export const getStaticProps: GetStaticProps = async () => {
-  const res = await fetch(
-    `https://api.airtable.com/v0/${process.env.base_id}/products?filterByFormula=AND(({isActive}=1))&api_key=${process.env.api_key}`
-  );
-  const recs = (await res.json()) as AirRecords;
+// export const getStaticProps: GetStaticProps = async () => {
+//   const res = await fetch(
+//     `https://api.airtable.com/v0/${process.env.base_id}/products?filterByFormula=AND(({isActive}=1))&api_key=${process.env.api_key}`
+//   );
+//   const recs = (await res.json()) as AirRecords;
 
-  //   const res = await fetch("http://localhost:3000/api/products");
+//   //   const res = await fetch("http://localhost:3000/api/products");
 
-  //   const recs = await res.json();
+//   //   const recs = await res.json();
 
-  return {
-    props: {
-      recs,
-    },
-  };
-};
+//   return {
+//     props: {
+//       recs,
+//     },
+//   };
+// };
 
-export default async function CatNav({ recs }: AirProps) {
+export default function CatNav({ recs }: AirProps) {
   // const CatNav = () => {
   // const res = await fetch("/api/products");
   // const recs = (await res.json()) as ProductApi[];
+
+  // const f = GetRecs();
 
   console.log({ catNav: recs });
   return <div>{JSON.stringify(recs)}t</div>;
