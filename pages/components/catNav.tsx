80a70fb529222f6458b24f6e094a42f141cea54e
alorslouis <alorslouis@users.noreply.github.com>
2022-10-12 15:07:01 -0700

    c: [catNav] - conditional handling for "active route" on link
    c: [catNav] - added a `router.asPath` then split to get active route (gross but works in dev) and added a "text-amber-" for active route

--- pages/components/catNav.tsx
+++ pages/components/catNav.tsx
@@ -1,6 +1,6 @@
 import { GetStaticProps } from "next";
 import Link from "next/link";
-import Router from "next/router";
+import Router, { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import { AirProps } from "../products/category/[category]";
 import { AirRecords } from "../products/[name]";
@@ -29,6 +29,11 @@ export default function CatNav({ recs }: AirProps) {
 
   // const f = GetRecs();
 
+  const router = useRouter();
+
+  const ff = router.asPath.split("/");
+  const cat = ff[ff.length - 1];
+  console.log(cat);
   console.log({ catNav: recs });
 
   const categories = new Set<string>();
@@ -37,19 +42,20 @@ export default function CatNav({ recs }: AirProps) {
   });
   const jj = Array.from(categories.values());
   return (
-    <div className="flex flex-col">
-      <hr />
-      <div className="flex sticky top-0 z-10 self-start">
-        {jj.map((j) => {
-          return (
-            <Link key={j} href={`/products/category/${j}`}>
-              <div className=" uppercase text-xs hover:scale-95 font-futura self-center bg-slate-600 dark:bg-slate-200 hover:backdrop-brightness-125 border-transparent backdrop-blur-lg first:rounded-bl-lg last:rounded-br-lg active:translate-y-1 active:shadow-lg border-b-2 hover:border-black dark:hover:border-b-white bg-opacity-10 dark:bg-opacity-10 px-4 py-4 cursor-pointer transition-all ease-in-out">
-                {j}
-              </div>
-            </Link>
-          );
-        })}
-      </div>
+    <div className="flex sticky flex-wrap top-0 z-10 self-start">
+      {jj.map((j) => {
+        return (
+          <Link key={j} href={`/products/category/${j}`}>
+            <div
+              className={`${
+                cat === j && "text-amber-400"
+              } uppercase text-xs hover:scale-95 font-futura self-center bg-slate-600 dark:bg-slate-200 hover:backdrop-brightness-125 border-transparent backdrop-blur-lg first:rounded-bl-lg last:rounded-br-lg active:translate-y-1 active:shadow-lg border-b-2 hover:border-black dark:hover:border-b-white bg-opacity-10 dark:bg-opacity-10 px-4 py-4 cursor-pointer transition-all ease-in-out`}
+            >
+              {j}
+            </div>
+          </Link>
+        );
+      })}
     </div>
   );
 }
