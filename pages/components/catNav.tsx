d0576feab20726a4ea7c257fbbc5b218a1fb7425
alorslouis <alorslouis@users.noreply.github.com>
2022-10-12 14:37:11 -0700

    Update catNav.tsx

--- pages/components/catNav.tsx
+++ pages/components/catNav.tsx
@@ -1,4 +1,5 @@
 import { GetStaticProps } from "next";
+import Link from "next/link";
 import Router from "next/router";
 import { useEffect, useState } from "react";
 import { AirProps } from "../products/category/[category]";
@@ -29,28 +30,49 @@ export default function CatNav({ recs }: AirProps) {
   // const f = GetRecs();
 
   console.log({ catNav: recs });
-  return <div>{JSON.stringify("hi jimothy")}</div>;
+
+  const categories = new Set<string>();
+  recs.records.forEach((e) => {
+    categories.add(e.fields.category);
+  });
+  const jj = Array.from(categories.values());
+  return (
+    <div className="flex flex-col">
+      <hr />
+      <div className="flex sticky top-0 z-10 self-start">
+        {jj.map((j) => {
+          return (
+            <Link key={j} href={`/products/category/${j}`}>
+              <div className=" uppercase text-xs hover:scale-95 font-futura self-center bg-slate-600 dark:bg-slate-200 hover:backdrop-brightness-125 border-transparent backdrop-blur-lg first:rounded-bl-lg last:rounded-br-lg active:translate-y-1 active:shadow-lg border-b-2 hover:border-black dark:hover:border-b-white bg-opacity-10 dark:bg-opacity-10 px-4 py-4 cursor-pointer transition-all ease-in-out">
+                {j}
+              </div>
+            </Link>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
-async function GetRecs() {
-  const res = await fetch("/api/products");
+// async function GetRecs() {
+//   const res = await fetch("/api/products");
 
-  const [data, setData] = useState(null);
-  useEffect(() => {
-    let ignore = false;
-    fetch("/api/products")
-      .then((response) => response.json())
-      .then((json) => {
-        if (!ignore) {
-          setData(json);
-        }
-      });
-    return () => {
-      ignore = true;
-    };
-  }, []);
-  return data;
-}
+//   const [data, setData] = useState(null);
+//   useEffect(() => {
+//     let ignore = false;
+//     fetch("/api/products")
+//       .then((response) => response.json())
+//       .then((json) => {
+//         if (!ignore) {
+//           setData(json);
+//         }
+//       });
+//     return () => {
+//       ignore = true;
+//     };
+//   }, []);
+//   return data;
+// }
 
 // export default CatNav;
 
