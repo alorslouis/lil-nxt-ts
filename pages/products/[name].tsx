5f30f51ffa5fcaf5dc0ca13675282c7d65e6bde2
alorslouis <alorslouis@users.noreply.github.com>
2022-12-04 15:43:42 -0800

    Update [name].tsx

--- pages/products/[name].tsx
+++ pages/products/[name].tsx
@@ -290,11 +290,11 @@ function Product({ post }: InferGetStaticPropsType<typeof getStaticProps>) {
               product?.fields?.attach[0]?.thumbnails?.large?.url
             }
             // weight is in grams
-            data-item-weight={200}
+            data-item-weight={"2"}
             // dimensions are in cm
-            data-item-width={12}
-            data-item-height={2}
-            data-item-length={12}
+            data-item-width={"12"}
+            data-item-height={"2"}
+            data-item-length={"12"}
             data-item-name={product.fields.title}
             // disabled={product.fields.inventory === 0}
             disabled={product.fields.inventory === 0 || !inStock}
