ca11bd5ea9683373ea8f8eaf2b59d3da7bc7af78
alorslouis <alorslouis@users.noreply.github.com>
2022-12-31 13:35:06 -0800

    Revert "Update _document.js"
    This reverts commit 94eda77a94ab8d8930dd9618f8899b15cd303d6a.

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
-import { Html, Head, Main } from "next/document";
+import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
@@ -14,7 +15,7 @@ export default function Document() {
       </Head>
       <body>
         <Main />
-
+        <NextScript />
         <script
           async
           dangerouslySetInnerHTML={{
